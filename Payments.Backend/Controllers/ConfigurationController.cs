c807b5fdda25f6971af498869479df86030df215
Matthew Casey <mdcasey@chabloom.com>
2020-08-08 18:15:29 -0400

    Add configuration controller

---
+++ Payments.Backend/Controllers/ConfigurationController.cs
@@ -0,0 +1,24 @@
+﻿// Copyright 2020 Chabloom LC. All rights reserved.
+
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Payments.Backend.Controllers
+{
+    public class ConfigurationController : Controller
+    {
+        public ConfigurationController(IClientRequestParametersProvider clientRequestParametersProvider)
+        {
+            ClientRequestParametersProvider = clientRequestParametersProvider;
+        }
+
+        public IClientRequestParametersProvider ClientRequestParametersProvider { get; }
+
+        [HttpGet("_configuration/{clientId}")]
+        public IActionResult GetClientRequestParameters([FromRoute] string clientId)
+        {
+            var parameters = ClientRequestParametersProvider.GetClientParameters(HttpContext, clientId);
+            return Ok(parameters);
+        }
+    }
+}
