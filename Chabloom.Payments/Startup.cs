e84558d8d8fbc739740b70f590df773e3863c5f8
Matthew Casey <mdcasey@chabloom.com>
2020-08-17 23:52:08 -0400

    Use Chabloom.Accounts for authentication

--- Chabloom.Payments/Startup.cs
+++ Chabloom.Payments/Startup.cs
@@ -1,23 +1,28 @@
 // Copyright 2020 Chabloom LC. All rights reserved.
 
+using System;
 using Chabloom.Payments.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace Chabloom.Payments
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
+            Environment = env;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -26,18 +31,57 @@ namespace Chabloom.Payments
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
 
-            services.AddCors(options =>
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.Authority = "https://localhost:44303";
+                    options.Audience = "Chabloom.Payments";
+                });
+
+            services.AddAuthorization(options =>
             {
-                options.AddPolicy("Development",
-                    builder =>
-                    {
-                        builder.AllowAnyOrigin();
-                        builder.AllowAnyMethod();
-                        builder.AllowAnyHeader();
-                    });
+                options.AddPolicy("ApiScope", policy =>
+                {
+                    policy.RequireAuthenticatedUser();
+                    policy.RequireClaim("scope", "Chabloom.Payments");
+                });
             });
 
             services.AddControllers();
+
+            if (Environment.IsDevelopment())
+            {
+                // Setup development CORS
+                services.AddCors(options =>
+                {
+                    options.AddPolicy("Development",
+                        builder =>
+                        {
+                            builder.WithOrigins("https://localhost:44334");
+                            builder.WithOrigins("http://localhost:3000");
+                            builder.WithOrigins("http://localhost:3001");
+                            builder.AllowAnyMethod();
+                            builder.AllowAnyHeader();
+                            builder.AllowCredentials();
+                        });
+                });
+
+                // Setup generated OpenAPI documentation
+                services.AddSwaggerGen(options =>
+                {
+                    options.SwaggerDoc("v1", new OpenApiInfo
+                    {
+                        Title = "Chabloom Payments",
+                        Description = "Chabloom Payments v1 API",
+                        Version = "v1"
+                    });
+                    options.AddSecurityDefinition("openid", new OpenApiSecurityScheme
+                    {
+                        Type = SecuritySchemeType.OpenIdConnect,
+                        OpenIdConnectUrl = new Uri("https://localhost:44303/.well-known/openid-configuration")
+                    });
+                });
+            }
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,23 +92,21 @@ namespace Chabloom.Payments
                 app.UseDeveloperExceptionPage();
                 app.UseDatabaseErrorPage();
                 app.UseCors("Development");
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                app.UseSwagger(options => options.RouteTemplate = "/swagger/{documentName}/chabloom-payments-api.json");
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/swagger/v1/chabloom-payments-api.json", "Chabloom Payments v1 API");
+                });
             }
 
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
 
             app.UseRouting();
 
             app.UseAuthentication();
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+            app.UseEndpoints(endpoints => { endpoints.MapControllers().RequireAuthorization("ApiScope"); });
         }
     }
 }
