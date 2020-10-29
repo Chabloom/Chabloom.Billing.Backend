b02efdb7ade65659b840c645bebfe17c31c12def
Matthew Casey <mdcasey@chabloom.com>
2020-10-28 22:09:49 -0400

    Add application insights

--- Chabloom.Payments/Startup.cs
+++ Chabloom.Payments/Startup.cs
@@ -77,6 +77,8 @@ namespace Chabloom.Payments
                         builder.AllowCredentials();
                     });
             });
+
+            services.AddApplicationInsightsTelemetry();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
