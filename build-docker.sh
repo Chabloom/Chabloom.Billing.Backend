0c3127c9efbeed395a4bd77338284bb065805708
Matthew Casey <mdcasey@chabloom.com>
2021-03-20 19:15:45 -0400

    Update docker build scripts

--- build-docker.sh
+++
@@ -1,4 +0,0 @@
-docker build -t chabloom-billing-backend:1.0.0 .
-docker save chabloom-billing-backend > chabloom-billing-backend.tar
-microk8s ctr image import chabloom-billing-backend.tar
-rm chabloom-billing-backend.tar
---
+++ docker-build.sh
@@ -0,0 +1,2 @@
+docker build -t chbprod.azurecr.io/chabloom-billing-backend:1 .
+docker push chbprod.azurecr.io/chabloom-billing-backend:1
