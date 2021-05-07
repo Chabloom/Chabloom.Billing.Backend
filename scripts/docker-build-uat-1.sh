389a6c406124bf64ba63ace01846522cb633b506
Matthew Casey <mdcasey@chabloom.com>
2021-05-06 23:46:29 -0400

    Update docker build scripts

--- scripts/docker-build-uat-1.sh
+++ scripts/docker-build-uat-1.sh
@@ -1,2 +1,4 @@
-docker build -t chbuat1.azurecr.io/chabloom-billing-backend:1 .
-docker push chbuat1.azurecr.io/chabloom-billing-backend:1
+timestamp=`date +%s`
+docker build -t chb-uat-1.chabloom.com:32000/chabloom-billing-backend:$timestamp -t chb-uat-1.chabloom.com:32000/chabloom-billing-backend:latest .
+docker push chb-uat-1.chabloom.com:32000/chabloom-billing-backend:$timestamp
+docker push chb-uat-1.chabloom.com:32000/chabloom-billing-backend:latest
--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,2 +1,4 @@
-docker build -t chbprod.azurecr.io/chabloom-billing-backend:1 .
-docker push chbprod.azurecr.io/chabloom-billing-backend:1
+timestamp=`date +%s`
+docker build -t chb-prod-1.chabloom.com:32000/chabloom-billing-backend:$timestamp -t chb-prod-1.chabloom.com:32000/chabloom-billing-backend:latest .
+docker push chb-prod-1.chabloom.com:32000/chabloom-billing-backend:$timestamp
+docker push chb-prod-1.chabloom.com:32000/chabloom-billing-backend:latest
