b24b2c634c93a680a0377d1dbce3957321857b54
Matthew Casey <mdcasey@chabloom.com>
2021-05-26 22:42:13 -0400

    Update tenants controller

--- Chabloom.Billing.Backend/Controllers/MultiTenant/TenantsController.cs
+++ Chabloom.Billing.Backend/Controllers/MultiTenant/TenantsController.cs
@@ -1,69 +1,41 @@
 ﻿// Copyright 2020-2021 Chabloom LC. All rights reserved.
 
 using System;
-using System.Linq;
 using System.Threading.Tasks;
 using Chabloom.Billing.Backend.Data;
 using Chabloom.Billing.Backend.Services;
 using Chabloom.Billing.Backend.ViewModels.MultiTenant;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
 
 namespace Chabloom.Billing.Backend.Controllers.MultiTenant
 {
-    [AllowAnonymous]
+    [Authorize]
     [ApiController]
     [Route("api/[controller]")]
     [Produces("application/json")]
     public class TenantsController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
-        private readonly ILogger<TenantsController> _logger;
         private readonly IValidator _validator;
 
-        public TenantsController(ApplicationDbContext context, ILogger<TenantsController> logger, IValidator validator)
+        public TenantsController(ApplicationDbContext context, IValidator validator)
         {
             _context = context;
-            _logger = logger;
             _validator = validator;
         }
 
+        [AllowAnonymous]
         [HttpGet("{id}")]
         [ProducesResponseType(200)]
-        [ProducesResponseType(401)]
-        [ProducesResponseType(403)]
         [ProducesResponseType(404)]
         public async Task<IActionResult> GetTenantAsync(Guid id)
         {
-            // Find the specified tenant
-            var viewModel = await _context.Tenants
-                .Select(x => new TenantViewModel
-                {
-                    Id = x.Id,
-                    Name = x.Name
-                })
-                .FirstOrDefaultAsync(x => x.Id == id);
-            if (viewModel == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(viewModel);
-        }
-
-        [HttpGet("Current")]
-        [ProducesResponseType(200)]
-        [ProducesResponseType(400)]
-        [ProducesResponseType(404)]
-        public async Task<IActionResult> GetCurrentTenantAsync()
-        {
-            // Get the current tenant
-            var tenant = await _validator.GetCurrentTenantAsync(Request);
+            // Find the tenant based on the tenant id
+            var tenant = await _context.Tenants
+                .FindAsync(id);
             if (tenant == null)
             {
-                _logger.LogWarning($"Could not find tenant for address");
                 return NotFound();
             }
 
@@ -74,6 +46,61 @@ namespace Chabloom.Billing.Backend.Controllers.MultiTenant
             };
 
             return Ok(retViewModel);
+        }
+
+        [AllowAnonymous]
+        [HttpGet("Current")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
+        public async Task<IActionResult> GetTenantCurrentAsync()
+        {
+            // Get the current tenant id
+            var tenantId = await _validator.GetTenantIdAsync(Request);
+            if (!tenantId.HasValue)
+            {
+                return NotFound();
+            }
+
+            // Find the tenant based on the tenant id
+            var tenant = await _context.Tenants
+                .FindAsync(tenantId);
+            if (tenant == null)
+            {
+                return NotFound();
+            }
+
+            var retViewModel = new TenantViewModel
+            {
+                Id = tenant.Id,
+                Name = tenant.Name
+            };
+
+            return Ok(retViewModel);
+        }
+
+        [HttpGet("Roles")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
+        public async Task<IActionResult> GetTenantRolesAsync()
+        {
+            // Get the user id
+            var userId = _validator.GetUserId(User);
+            if (!userId.HasValue)
+            {
+                return Forbid();
+            }
+
+            // Get the current tenant id
+            var tenantId = await _validator.GetTenantIdAsync(Request);
+            if (!tenantId.HasValue)
+            {
+                return NotFound();
+            }
+
+            // Get tenant roles for the user
+            var tenantRoles = await _validator.GetTenantRolesAsync(userId.Value, tenantId.Value);
+
+            return Ok(tenantRoles);
         }
     }
 }
