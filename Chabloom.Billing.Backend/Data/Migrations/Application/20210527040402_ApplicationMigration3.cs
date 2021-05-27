bf33f10691188196aeab15f100a27a426fe2fae0
Matthew Casey <mdcasey@chabloom.com>
2021-05-27 00:05:19 -0400

    Add migration

---
+++ Chabloom.Billing.Backend/Data/Migrations/Application/20210527040402_ApplicationMigration3.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Chabloom.Billing.Backend.Data.Migrations.Application
+{
+    public partial class ApplicationMigration3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Accounts_TenantId",
+                table: "Accounts");
+
+            migrationBuilder.AddUniqueConstraint(
+                name: "AK_Accounts_TenantId_ReferenceId",
+                table: "Accounts",
+                columns: new[] { "TenantId", "ReferenceId" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropUniqueConstraint(
+                name: "AK_Accounts_TenantId_ReferenceId",
+                table: "Accounts");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Accounts_TenantId",
+                table: "Accounts",
+                column: "TenantId");
+        }
+    }
+}
